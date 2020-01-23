fd602a47d053acd6fe69af030636318a7fc2960b
Pat Brown <ptrckbrown94@gmail.com>
2020-01-23 16:33:00 -0600

    working on the form/list for employees

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+//  import Header from "./Components/"
+import List from "./Components/List"
+import API from "./utils/api.js"
 
-function App() {
+class App extends Component() {
+  state={
+    users:[]
+
+  }
+
+  componentDidMount(){
+  
+    // call the api for get the users
+    const getusers = API.search()
+    this.setState({users: getusers})
+  }
+
+
+
+ render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+     <List 
+     users={this.state.users}
+     />
+
     </div>
-  );
+  )
+}
 }
 
 export default App;
---
+++ src/Components/List.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+function List(props){
+
+return(
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">DOB</th>
+      <th scope="col">City</th>
+      <th scope="col">email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+    <td ></td>
+      <td >First</td>
+      <td >Last</td>
+      <td >DOB</td>
+      <td >City</td>
+      <td >email</td>
+    </tr>
+    
+  </tbody>
+</table>
+)
+
+}
+export default List;
