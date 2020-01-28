7a1fea5564652901cded2b031b09675772ce3f18
Pat Brown <ptrckbrown94@gmail.com>
2020-01-28 12:52:11 -0600

    updated sort and fixed spelling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <div>
             <h1>Employee Directory</h1>
             <h3>Search by Employee first name</h3>
-            <h3>Filter by age</h3>
+            <h3>Sort by age</h3>
         </div>
     )
 
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+// Austin Schlander helped with the sorting function
+// const AgeSort = property => {
+//     var sortOrder = 1;
+//     if(property[0] === "-") {
+//         sortOrder = -1;
+//         property = property.substr(1);
+//     }
+//     return function (a,b) {
+//         /* next line works with strings and numbers, 
+//          * and you may want to customize it to your needs
+//          */
+//         var result = (a.dob[property] < b.dob[property]) ? -1 : (a.dob[property] > b.dob[property]) ? 1 : 0;
+//         return result * sortOrder;
+//     }
+// };
 
 function List(props) {
 
@@ -9,7 +24,7 @@ function List(props) {
                 <tr>                    
                     <th scope="col">First</th>
                     <th scope="col">Last</th>
-                    <th scope="col"><button>Age</button></th> 
+                    <th scope="col">Age</th> 
                     <th scope="col">City</th>
                     <th scope="col">email</th>
                 </tr>
