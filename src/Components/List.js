0301fcbb7f74f9efc90c147da10fab1d45ae8c84
Pat Brown <ptrckbrown94@gmail.com>
2020-01-29 09:58:02 -0600

    deleted unneeded code

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,20 +1,6 @@
 import React from "react";
 
-// Austin Schlander helped with the sorting function
-// const AgeSort = property => {
-//     var sortOrder = 1;
-//     if(property[0] === "-") {
-//         sortOrder = -1;
-//         property = property.substr(1);
-//     }
-//     return function (a,b) {
-//         /* next line works with strings and numbers, 
-//          * and you may want to customize it to your needs
-//          */
-//         var result = (a.dob[property] < b.dob[property]) ? -1 : (a.dob[property] > b.dob[property]) ? 1 : 0;
-//         return result * sortOrder;
-//     }
-// };
+
 
 // the below function is used to render the the information from the API 
 
